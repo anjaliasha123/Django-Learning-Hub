cf2eada991d1144513a981fc6c8caf8cf97560f4
anjaliasha123 <anjaliasha95@gmail.com>
2024-12-19 09:16:00 -0600

    permissions update delete CBV

--- Project2/api/views.py
+++ Project2/api/views.py
@@ -2,39 +2,98 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.db.models.aggregates import Max
 from rest_framework.views import Response
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.decorators import api_view
 from api.serializers import OrderSerializer, ProductSerializer, ProductInfoSerializer
 from api.models import Product, Order, OrderItem
 
-@api_view(['GET'])
-def product_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    # return JsonResponse({
-    #     'data': serializer.data
-    # })
-    return Response(serializer.data)
 
-@api_view(['GET', 'POST', 'PUT'])
-def product_detail(request, pk):
-    # product = Product.objects.get(pk=pk)
-    product = get_object_or_404(klass=Product,pk=pk)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
 
-@api_view(['GET'])
-def order_list(request):
-    orders = Order.objects\
-        .prefetch_related('items__product')
-    serializer = OrderSerializer(orders, many=True)
-    return Response(serializer.data)
+class ProductCreateAPIView(generics.CreateAPIView):
+    model = Product
+    serializer_class = ProductSerializer
 
-@api_view(['GET'])
-def product_info(request):
-    products = Product.objects.all()
-    serializer = ProductInfoSerializer({
-        'products': products,
-        'count' : len(products),
-        'max_price': products.aggregate(max_price=Max('price'))['max_price']
-    })
-    return Response(serializer.data)
+class ProductListAndCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+        if self.request.method == 'POST':
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
+
+# @api_view(['GET'])
+# def product_list(request):
+#     products = Product.objects.all()
+#     serializer = ProductSerializer(products, many=True)
+#     # return JsonResponse({
+#     #     'data': serializer.data
+#     # })
+#     return Response(serializer.data)
+
+class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_url_kwarg = 'product_id'
+
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+        if self.request.method in ['POST', 'PUT', 'DELETE', 'PATCH'] :
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
+# @api_view(['GET', 'POST', 'PUT'])
+# def product_detail(request, pk):
+#     # product = Product.objects.get(pk=pk)
+#     product = get_object_or_404(klass=Product,pk=pk)
+#     serializer = ProductSerializer(product)
+#     return Response(serializer.data)
+
+class OrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related('items__product')
+    serializer_class = OrderSerializer
+
+# to get orders only if the user with permission
+class UserOrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related('items__product')
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        user = self.request.user
+        qs = super().get_queryset()
+        return qs.filter(user=user)
+
+# def get_queryset(self):
+#     user = self.request.user
+#     return user.accounts.all()
+# @api_view(['GET'])
+# def order_list(request):
+#     orders = Order.objects\
+#         .prefetch_related('items__product')
+#     serializer = OrderSerializer(orders, many=True)
+#     return Response(serializer.data)
+class ProductInfoAPIView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductInfoSerializer({
+            'products': products,
+            'count' : len(products),
+            'max_price': products.aggregate(max_price=Max('price'))['max_price']
+        })
+        return Response(serializer.data)
+
+# @api_view(['GET'])
+# def product_info(request):
+#     products = Product.objects.all()
+#     serializer = ProductInfoSerializer({
+#         'products': products,
+#         'count' : len(products),
+#         'max_price': products.aggregate(max_price=Max('price'))['max_price']
+#     })
+#     return Response(serializer.data)
