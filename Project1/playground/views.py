1d076daffc5f48f3b0d40878f7b7347b427628e7
anjaliasha123 <anjaliasha95@gmail.com>
2024-12-31 23:05:53 -0600

    celery & beats

--- Project1/playground/views.py
+++ Project1/playground/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render
-from django.core.mail import send_mail, mail_admins , BadHeaderError, EmailMessage
-from templated_mail.mail import BaseEmailMessage
+from .tasks import notify_customers
+# from django.core.mail import send_mail, mail_admins , BadHeaderError, EmailMessage
+# from templated_mail.mail import BaseEmailMessage
 
 
 # Create your views here.
 
 def hello(request):
-    try:
+    # try:
         # send_mail('test mail', 'hello from me', 'info@domain.com', ['bob@moshy.com'])
         # message = EmailMessage('test mail', 'hello from me', from_email='test@mail.com', to=['annu@microsoft.com'])
         # message.attach_file('playground/static/images/doh.png')
         # message.send()
-        message = BaseEmailMessage(
-            template_name='emails/hello.html',
-            context={'name': 'annu'}
-        )
-        message.send(['fake@mail.com'])
-    except BadHeaderError:
-        pass
+    #     message = BaseEmailMessage(
+    #         template_name='emails/hello.html',
+    #         context={'name': 'annu'}
+    #     )
+    #     message.send(['fake@mail.com'])
+    # except BadHeaderError:
+    #     pass
+    notify_customers.delay('Hello')
     return render(request, 'hello.html', {'name': 'Anna'})
--- Project1/storefront/settings.py
+++ Project1/storefront/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -187,3 +188,13 @@ DEFAULT_FROM_EMAIL = 'from@domain.com'
 ADMINS = [
     ('Annu', 'annu@mail.com')
 ]
+
+CELERY_BROKER_URL = 'redis://localhost:6379/1'
+CELERY_BEAT_SCHEDULE = {
+    'notify_customers': {
+        'task' : 'playground.tasks.notify_customers',
+        'schedule' : 5,
+        'args': ['Celery Beats Here...........'],
+    }
+}
+# crontab(minute='*/2')
