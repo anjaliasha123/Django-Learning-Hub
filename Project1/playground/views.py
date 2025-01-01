677eac46ed5da7010c3ae3faa389f0ed7e94b32c
anjaliasha123 <anjaliasha95@gmail.com>
2025-01-01 17:42:21 -0600

    caching-redis

--- Project1/playground/views.py
+++ Project1/playground/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
-from .tasks import notify_customers
+import requests
+from django.utils.decorators import method_decorator
+from rest_framework.views import APIView
+from django.views.decorators.cache import cache_page
+# from .tasks import notify_customers
+# from django.core.cache import cache
 # from django.core.mail import send_mail, mail_admins , BadHeaderError, EmailMessage
 # from templated_mail.mail import BaseEmailMessage
 
+class HelloView(APIView):
+    @method_decorator(cache_page(5*60))
+    def get(self, request):
+        response = requests.get('https://httpbin.org/delay/2')
+        data = response.json()
+        return render(request, 'hello.html', {'name': data})
 
 # Create your views here.
-
-def hello(request):
+# @cache_page(5*60)
+# def hello(request):
+#     key = 'httpbin_result'
     # try:
         # send_mail('test mail', 'hello from me', 'info@domain.com', ['bob@moshy.com'])
         # message = EmailMessage('test mail', 'hello from me', from_email='test@mail.com', to=['annu@microsoft.com'])
@@ -19,5 +31,13 @@ def hello(request):
     #     message.send(['fake@mail.com'])
     # except BadHeaderError:
     #     pass
-    notify_customers.delay('Hello')
-    return render(request, 'hello.html', {'name': 'Anna'})
+    # notify_customers.delay('Hello')
+    # if cache.get(key) is None:
+    #     response = requests.get('https://httpbin.org/delay/2')
+    #     data = response.json()
+    #     cache.set(key, data)
+    # using caching views using decorator
+    # response = requests.get('https://httpbin.org/delay/2')
+    # data = response.json()
+    # # return render(request, 'hello.html', {'name': cache.get(key)})
+    # return render(request, 'hello.html', {'name': data})
--- Project1/storefront/settings.py
+++ Project1/storefront/settings.py
@@ -204,3 +204,14 @@ CELERY_BEAT_SCHEDULE = {
     }
 }
 # crontab(minute='*/2')
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/2",
+        "TIMEOUT": 10 * 60,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
